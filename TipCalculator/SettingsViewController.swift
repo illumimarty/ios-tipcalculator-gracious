14ede311cec21995127041566536038ce0cf79bf
Marthen Nodado <mnodado@ucmerced.edu>
2021-01-02 13:38:34 -0800

    created a settings page and corresponding view controller.

--- TipCalculator/SettingsViewController.swift
+++ TipCalculator/SettingsViewController.swift
@@ -9,12 +9,38 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         self.title = "Da Settings"
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+    
+        //let tipPercentage = defaults.double(forKey: "myDouble")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+        let defaults = UserDefaults.standard
+        defaults.synchronize()
+    
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+        let defaults = UserDefaults.standard
+
+        let tipPercentages = [0.15, 0.18, 0.2]
+        defaults.set(tipPercentages[tipControl.selectedSegmentIndex], forKey: "tipPercentage")
+        defaults.synchronize()
+    }
+    
+    
     
 
     /*
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -8,16 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipPercentageLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    var tipPercentage: Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         self.title = "Tip Calculator"
+        
+        let defaults = UserDefaults.standard
+        defaults.set(0.15, forKey: "tipPercentage")
+        defaults.synchronize()
     }
     @IBAction func onTap(_ sender: Any) {
         print("Hello world")
@@ -27,12 +31,16 @@ class ViewController: UIViewController {
     
     @IBAction func calculateTip(_ sender: Any) {
         
+        //let defaults = UserDefaults.standard
         // Get the bill amount
         let bill = Double(billAmountTextField.text!) ?? 0
-        let tipPercentages = [0.15, 0.18, 0.2]
+        // let tipPercentages = [0.15, 0.18, 0.2]
+        
+        
+        
         // Calculate the tip and total
         
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let tip = bill * tipPercentage
         let total = bill + tip
         // Update the tip and total labels
         tipPercentageLabel.text = String(format: "$%.2f", tip)
@@ -42,8 +50,15 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("view will appear")
+    
+        //let tipPercentage = defaults.double(forKey: "myDouble")
         // This is a good place to retrieve the default tip percentage from UserDefaults
         // and use it to update the tip amount
+        let defaults = UserDefaults.standard
+        tipPercentage = defaults.double(forKey: "tipPercentage")
+        defaults.synchronize()
+
+        calculateTip((Any).self)
     }
 
     override func viewDidAppear(_ animated: Bool) {
